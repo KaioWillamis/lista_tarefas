a93afa2b783b746136d58c9cd8130ee647074561
KaioWillamis <brm560548@gmail.com>
2024-12-09 10:31:22 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const abrir_navLateral = document.querySelector(".botton_list");
 const menu_lateral = document.querySelector("#menu_lateral");
 const fechar_navLateral = document.querySelector(".fechar_navLateral");
-//const modal = document.querySelector("#modal");
-//const abrir_modal = document.querySelector(".abrir_modal");
-//const fechar_modal = document.querySelector(".fechar_modal");
+const modal = document.querySelector("#modal");
+const abrir_modal = document.querySelector(".abrir_modal");
+const fechar_modal = document.querySelector(".fechar_modal");
 
-//abrir_modal.addEventListener("click", function(){
-    //modal.style.display = "flex";
-//});
+abrir_modal.addEventListener("click", function(){
+    modal.style.display = "flex";
+});
 
-//fechar_modal.addEventListener("click", function(){
-    //modal.style.display = "none";
-//});
+fechar_modal.addEventListener("click", function(){
+    modal.style.display = "none";
+});
 
 abrir_navLateral.addEventListener("click",function(){
     menu_lateral.style.display = "flex";
