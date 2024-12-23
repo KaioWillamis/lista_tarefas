84113a5c5cf47cdeaeab2ce28301050e94342035
KaioWillamis <brm560548@gmail.com>
2024-12-23 19:19:28 -0300

    atualizei

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,10 @@ const esqueci_senha = document.querySelector("#esqueci_senha");
 /*Variaveis do input de Login*/
 const input_login_senha = document.querySelector("#senha");
 const icone_senha_login = document.querySelector("#icone_senha_login");
+const input_cadastro_login = document.querySelector("#senhaCadastrar");
+const icone_cadastrar_senha = document.querySelector("#icone_cadastrar_senha");
+const input_confirmar_senha = document.querySelector("#confirmarSenhaCadastrar");
+const icone_confirmar_senha = document.querySelector("#icone_confirmar_senha");
 
 /* Função para atualizar título da Lista */
 titulos_lista.forEach(function(titulo_lista) {
@@ -72,6 +76,7 @@ fechar_modal.forEach((btn) =>{
         modal.style.display = "none";
         conteudo_modal.style.display = "flex";
         cadastrarLogin.style.display = "none";
+        olhoFechado();
     });
 });
 
@@ -83,6 +88,8 @@ voltarModal.forEach((btn) => {
 
         // Exibe o modal principal (conteudo_modal)
         conteudo_modal.style.display = "flex";
+        
+        olhoFechado();
     });
 });
 
@@ -117,14 +124,34 @@ meu_desempenho.forEach((btn) =>{
 
 /*Funções do Form de Login*/
 icone_senha_login.addEventListener("click", function(){
-    if(input_login_senha.type === "password"){
-        input_login_senha.type = "text";
-        icone_senha_login.classList.remove("bi-eye");
-        icone_senha_login.classList.add("bi-eye-slash");
+    trocar_icone_olho(input_login_senha,icone_senha_login);
+});
+
+icone_cadastrar_senha.addEventListener("click", function(){
+    trocar_icone_olho(input_cadastro_login,icone_cadastrar_senha);
+});
+
+icone_confirmar_senha.addEventListener("click", function(){
+    trocar_icone_olho(input_confirmar_senha,icone_confirmar_senha);
+});
+
+function trocar_icone_olho(input,icone){
+    if(input.type === "password"){
+        input.type = "text";
+        icone.classList.remove("bi-eye");
+        icone.classList.add("bi-eye-slash");
     }
     else{
-        input_login_senha.type = "password";
-        icone_senha_login.classList.remove("bi-eye-slash");
-        icone_senha_login.classList.add("bi-eye");
+        input.type = "password";
+        icone.classList.remove("bi-eye-slash");
+        icone.classList.add("bi-eye");
     }
-});
+}
+
+function olhoFechado(){
+    icone_senha_login.classList = "bi-eye";
+    icone_cadastrar_senha.classList = "bi-eye";
+    icone_confirmar_senha.classList = "bi-eye";
+}
+
+/*Fim das funções do form de login*/
